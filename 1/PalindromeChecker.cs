c67bbc0e71d4de237a3b26f4bcd530db81649406
Porchuk <porchuk.ivan@chnu.edu.ua>
2025-06-27 09:07:20 +0300

    Add files via upload

---
+++ 1/PalindromeChecker.cs
@@ -0,0 +1,34 @@
+﻿
+namespace Practics1
+{
+    public class PalindromeChecker
+    {
+        public bool IsPalindrome(string input)
+        {
+            if (input == null || input.Equals(""))
+            {
+                return false;
+            }
+            string s = new string(input.ToCharArray()
+                .Where(c => Char.IsAsciiLetter(c))
+                .ToArray());
+           
+            s = s.ToLower();
+            char[] charArray = s.ToCharArray();
+            Array.Reverse(charArray);
+            string s2 = new string(charArray);
+
+            Console.WriteLine(s);
+            Console.WriteLine(s2);
+            
+            if (s2 == s) { 
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+
+}
---
+++ 1/TemperatureConverter.cs
@@ -0,0 +1,17 @@
+﻿
+namespace Practics1
+{
+    public class TemperatureConverter
+    {
+        public double FahrenheitToCelsius(double temperature)
+        {
+            return (temperature - 32) * 5/9;
+        }
+
+        public double CelsiusToFahrenheit(double temperature)
+        {
+            return temperature * 9/5 + 32;
+        }
+    }
+
+}
